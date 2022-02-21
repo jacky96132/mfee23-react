18b594b6684f012871723df20164b5fd779349a9
Anna <anna@joomla.com.tw>
2022-02-21 15:25:03 +0800

    AD

---
+++ 程式碼/0221/9.props-實例-react-share套件/App.js
@@ -0,0 +1,11 @@
+import SocialShare from './components/SocialShare'
+
+function App() {
+  return (
+    <>
+      <SocialShare url="http://localhost:3000" round={false} />
+    </>
+  )
+}
+
+export default App
