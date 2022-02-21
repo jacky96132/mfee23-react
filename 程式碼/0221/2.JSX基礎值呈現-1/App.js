a3e7e3c1e5bdf0c1a737cfbb4f9d8a26a6ff630f
Anna <anna@joomla.com.tw>
2022-02-21 10:06:30 +0800

    A

---
+++ 程式碼/0221/2.JSX基礎值呈現-1/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2>Number</h2>
+      {123}
+      {123 + 1}
+      <h2>Boolean</h2>
+      {true}
+      {false}
+      <h2>String</h2>
+      {'abc'}
+      {'abc' + 'de'}
+      <h2>null/undefined</h2>
+      {null}
+      {undefined}
+    </>
+  )
+}
+
+export default App
