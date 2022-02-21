f4387bd51583b5edc2b4b5d1a5f6d9b8fd84056d
Anna <anna@joomla.com.tw>
2022-02-21 11:08:29 +0800

    a

--- 程式碼/0221/4.JSX語法map範例-1/App.js
+++ 程式碼/0221/4.JSX語法map範例-1/App.js
@@ -1,6 +1,10 @@
+// 導入一般的js物件檔
 import { data } from './data/student'
+// 導入json一樣會自動轉為js 物件
+import students from './data/student.json'
 
 function App() {
+  console.log(students)
   console.log(data)
 
   const newData = data.map((v, i) => {
