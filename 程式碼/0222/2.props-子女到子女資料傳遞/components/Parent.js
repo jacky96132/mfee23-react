b05bc3bd3b2fd9a359f8bbacd9a370d4c17b6e98
Anna <anna@joomla.com.tw>
2022-02-22 10:25:11 +0800

    A

---
+++ 程式碼/0222/2.props-子女到子女資料傳遞/components/Parent.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import ChildA from './ChildA'
+import ChildB from './ChildB'
+
+function Parent() {
+  //給ChildB設定回資料用的狀態
+  const [data, setData] = useState('')
+
+  return (
+    <>
+      <h2>Parent</h2>
+      <ChildB setData={setData} />
+      <ChildA data={data} />
+    </>
+  )
+}
+
+export default Parent
