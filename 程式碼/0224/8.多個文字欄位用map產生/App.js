950e49f55d2a43659d1b2ae09125308b43fd8aff
Eddy Chang <eddy@joomla.com.tw>
2022-02-24 14:02:10 +0800

    a

---
+++ 程式碼/0224/8.多個文字欄位用map產生/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import FCFormFieldsMapDemo from './components/FCFormFieldsMapDemo'
+
+function App() {
+  return (
+    <>
+      <FCFormFieldsMapDemo />
+    </>
+  )
+}
+
+export default App
